ae5cf8e64ecd876a8dbbf10da949a47676c730fc
7EHR4F <raphael.schumacher@fonxio.com>
2014-04-14 22:17:36 +0200

    menu project
    scripts are in javascript but very simples so no performance impact. C#
    for game and Js for menus seems to be a good idea. Unity can also use
    css rules for some design, seems to be easier with Js.

---
+++ menu/Assets/under_quit.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+var isQuitButton = false;
+ GameObject.Find("under_quit").renderer.enabled = false;
+function OnMouseEnter()
+{
+ GameObject.Find("under_quit").renderer.enabled = true;
+
+}
+
+ 
+
+function OnMouseExit()
+{ 
+ GameObject.Find("under_quit").renderer.enabled = false;
+
+ }
+
+ function OnMouseUp()
+
+{
+
+    //Are We Dealing With A Quit Button
+
+    if( isQuitButton )
+
+    {
+print ("You clicked the button Quit!");
+        //QuitTheGame
+
+        Application.Quit();
+
+    }
+}
